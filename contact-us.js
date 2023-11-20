085485c9ee0362ff3b50317a367f2d45d2da6353
Tung Ly <tungply7@gmail.com>
2023-11-20 14:30:33 -0800

    a

--- contact-us.js
+++ contact-us.js
@@ -1,31 +1,40 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyApS2jsMnIIFwjWky2vx0j001p72nvQv-U",
-    authDomain: "woofmeowcenter-3ec1a.firebaseapp.com",
-    projectId: "woofmeowcenter-3ec1a",
-    databaseURL: "https://woofmeowcenter-3ec1a-default-rtdb.firebaseio.com/",
-    storageBucket: "woofmeowcenter-3ec1a.appspot.com",
-    messagingSenderId: "49546601461",
-    appId: "1:49546601461:web:d579ee1f7201ddb9bef6bc"
-  };
+  apiKey: "AIzaSyApS2jsMnIIFwjWky2vx0j001p72nvQv-U",
+  authDomain: "woofmeowcenter-3ec1a.firebaseapp.com",
+  projectId: "woofmeowcenter-3ec1a",
+  databaseURL: "https://woofmeowcenter-3ec1a-default-rtdb.firebaseio.com/",
+  storageBucket: "woofmeowcenter-3ec1a.appspot.com",
+  messagingSenderId: "49546601461",
+  appId: "1:49546601461:web:d579ee1f7201ddb9bef6bc"
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  const dbRef = firebase.database().ref();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const detailsRef = dbRef.child('userdetails');
+// Reference to the Firebase database
+const database = firebase.database();
 
-function submitForm(){
-var name = document.getElementById("name").value;
-var email = document.getElementById("email").value;
-var message = document.getElementById("message").value;
-detailsRef.push().set({
-name: name,
-email: email,
-message: message,
+// Reference to the 'contacts' node in the database
+const contactsRef = database.ref('contacts');
+
+// Handle form submission
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Get form values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Push the form data to the 'contacts' node in the database
+  contactsRef.push({
+    name: name,
+    email: email,
+    message: message
+  });
+
+  // Clear the form after submission
+  document.getElementById('contactForm').reset();
+
+  alert('Form submitted successfully!');
 });
-}
-.then(function () {
-  // Display thank you message and hide the form
-  document.getElementById("formContainer").style.display = "none";
-  document.getElementById("thankYouMessage").style.display = "block";
-})
-
