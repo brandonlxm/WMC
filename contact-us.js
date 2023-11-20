3b621058b69eef3809965a66e974ca03182a16a8
Tung Ly <tungply7@gmail.com>
2023-11-20 13:58:11 -0800

    Create contact-us.js

---
+++ contact-us.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApS2jsMnIIFwjWky2vx0j001p72nvQv-U",
+    authDomain: "woofmeowcenter-3ec1a.firebaseapp.com",
+    projectId: "woofmeowcenter-3ec1a",
+    databaseURL: "https://woofmeowcenter-3ec1a-default-rtdb.firebaseio.com/",
+    storageBucket: "woofmeowcenter-3ec1a.appspot.com",
+    messagingSenderId: "49546601461",
+    appId: "1:49546601461:web:d579ee1f7201ddb9bef6bc"
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const dbRef = firebase.database().ref();
+
+  const detailsRef = dbRef.child('userdetails');
+detailsRef.on("child_added", function(snapshot, prevChildKey) {
+var newPost = snapshot.val();
+});
+function submitForm(){
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var message = document.getElementById("message").value;
+detailsRef.push().set({
+name: name,
+email: email,
+message: message,
+});
+}
